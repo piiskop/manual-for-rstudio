fdc257fb4231feb0a17851b960e0fa3e69d65468
peacecop kalmer <kalmer@test.tennis24.ee>
2022-11-08 23:25:21 +0200

    presentation

--- js.js
+++ js.js
@@ -1,3 +1,24 @@
+/**
+ * This function creates a link.
+ * 
+ * @param string div the <code>div</code> the link should be appended to
+ * @param string id the identifier of the link anchor
+ * @param string href the hyperlink
+ * @param string text the link text
+ */
+function createLink(div, id, href, text) {
+  let a = document.createElement("a");
+  a.setAttribute("id", id);
+  a.setAttribute("href", href);
+  a.innerHTML = text;
+  let p = document.createElement("p");
+  p.appendChild(a);
+  div.appendChild(p);
+}
+
+/**
+ * This function manages targets and links.
+ */
 function setTargets() {
 
   let divs = document.getElementsByClassName("csl-entry");
@@ -17,13 +38,9 @@ function setTargets() {
   document.getElementById("book-source").target = "GitHub";
   
   let div = document.getElementById("main-nav").getElementsByTagName("nav")[0].getElementsByClassName("book-extra")[0];
-  let a = document.createElement("a");
-  a.setAttribute("id", "book-print");
-  a.setAttribute("href", "manual-for-rstudio.pdf");
-  a.innerHTML = "View printable book ";
-  let p = document.createElement("p");
-  p.appendChild(a);
-  div.appendChild(p);
+  createLink(div, "book-print", "en.pdf", "View printable book");
+  let nameOfPresentation = "presentation";
+  createLink(div, "presentation-" + nameOfPresentation, nameOfPresentation + ".html", "Presentation");
 }
 
 window.onload = function () {
