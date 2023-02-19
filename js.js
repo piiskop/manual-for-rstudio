bf74fe8c593af11c8adfbce51dfbc4db64779720
peacecop kalmer <kalmer@test.tennis24.ee>
2023-02-19 21:26:19 +0200

    remote controlling presentation, multiline title, text size, chemical reaction arrow, site serving automation

--- js.js
+++ js.js
@@ -40,7 +40,8 @@ function setTargets() {
   let div = document.getElementById("main-nav").getElementsByTagName("nav")[0].getElementsByClassName("book-extra")[0];
   createLink(div, "book-print", "manual-for-rstudio.pdf", "View printable book");
   let nameOfPresentation = "presentation";
-  createLink(div, "presentation-" + nameOfPresentation, nameOfPresentation + ".html", "Presentation");
+  createLink(div, "presentation-" + nameOfPresentation, "qmd/" + nameOfPresentation + "/" + nameOfPresentation + ".html", "Presentation");
+  createLink(div, "presentation-speaker-" + nameOfPresentation, "qmd/" + nameOfPresentation + "/" + nameOfPresentation + "-speaker.html", "Remote control");
 }
 
 window.onload = function () {
